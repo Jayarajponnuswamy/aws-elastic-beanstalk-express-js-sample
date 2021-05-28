81b4def67c6abf2839078de01b02bce584763353
Jayarajponnuswamy <Jayarajmca1@gmail.com>
2021-05-28 22:05:23 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!.. Well come to AWS world'));
+app.get('/', (req, res) => res.send('Hello World!.. Well come to AWS world Jayaraj'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
